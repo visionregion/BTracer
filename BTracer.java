6ead07a5761363ce6aa484b5f75dcdf873c7de6e
visionregion <visionregion@gmail.com>
2017-12-13 15:48:37 +0200

    v0.1

--- BTracer.java
+++ BTracer.java
@@ -1,18 +1,40 @@
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Properties;
 
 public class BTracer {
     public static void main (String[] args) {
         String aclLine;
-        String path;
+        String path="";
         String findWord;
         String sRegex;
         Pattern pattern;
         Matcher replaced;
         int count;
 
-        path = "D:/temp/acl.txt";
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("resources/config.properties");
+            prop.load(input);
+
+            //System.out.println(prop.getProperty("path"));
+            path = prop.getProperty("path");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         findWord = "deny";
         sRegex = "(.*)"+"("+findWord+")"+"(.*)";
         pattern = Pattern.compile("[a]+");
